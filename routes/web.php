e4ad3264e0bd4dcc4f0e58ca69e0e1f8398eb630
Matias Olivera <matu120686@gmail.com>
2022-01-29 13:49:35 -0300

    creo rutas inciales para proyecto

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('main');
+
+Route::get('products', function () {
+    return "Esta es la Ruta de Productos";
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return "Esta es la Ruta de Crear un Producto";
+})->name('products.create');
+
+Route::post('products', function () {
+    //
+})->name('products.store');
+
+Route::get('products/{product}', function ($product) {
+    return "Esta es la Ruta de Mostrar un Producto: {$product}";
+})->name('products.show');
+
+Route::get('products/{product}/edit', function ($product) {
+    return "Esta es la Ruta de Editar un Producto: {$product}";
+})->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', function ($product) {
+    //
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+    //
+})->name('products.destroy');
